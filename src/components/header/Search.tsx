efe25102d079ece82e2d87aa997198489d66717d
Olha Vadiasova <olha.vadiasova@gmail.com>
2022-10-16 23:07:43 +0300

    TS-45 Implement search functionality by url

--- src/components/header/Search.tsx
+++ src/components/header/Search.tsx
@@ -1,24 +1,46 @@
-import { useContext, useState } from "react";
+import React, { useState, useEffect } from 'react'
+import { useNavigate, Navigate  } from 'react-router-dom'
+
 import { Menu, MenuItem, Button } from "@mui/material";
 import { KeyboardArrowDown, KeyboardArrowUp } from "@mui/icons-material";
 
 import { searchIcon } from "../../assets";
-import { SearchContext } from "../../context/SearchContext";
 
 export default function SearchBar() {
-  const { value, setValue } = useContext(SearchContext);
+  const navigate = useNavigate();
+
+  const [searchValue, setSearchValue] = useState('')
+  const [assetTokenId, setAssetTokenId] = useState('')
+  const [assetContract, setAssetContract] = useState('')
+
+  useEffect(() => {
+    if (!searchValue) {
+      return
+    }
+
+    setAssetContract(searchValue.split('/').slice(-2, -1)[0])
+    setAssetTokenId(searchValue.substring(searchValue.lastIndexOf('/') + 1))
+
+  }, [searchValue])
+
+  const handleSubmit = () => {
+    navigate(`/search-asset/${assetContract}/${assetTokenId}`)
+  }
+
   return (
-    <div className="search-box">
-      <img src={searchIcon} alt="Search Icon" />
-      <input
-        type="text"
-        placeholder="Asset URL / Asset ID / Contract / Keyword"
-        onChange={(event) => {
-          setValue({ ...value, term: event.target.value });
-        }}
-      />
-      <BasicMenu />
-    </div>
+    <form onSubmit={handleSubmit}>
+      <div className="search-box">
+        <img src={searchIcon} alt="Search Icon" />
+        <input
+          type="text"
+          placeholder="Asset URL / Asset ID / Contract / Keyword"
+          onChange={(ev) => {
+            setSearchValue(ev.target.value)
+          }}
+        />
+        <BasicMenu />
+      </div>
+    </form>
   );
 }
 
@@ -40,7 +62,7 @@ function BasicMenu() {
   };
 
   return (
-    <div className="search-filter">
+    <>
       <Button
         id="basic-button"
         aria-controls={open ? "basic-menu" : undefined}
@@ -65,6 +87,6 @@ function BasicMenu() {
         <MenuItem onClick={handleSelect}>Asset ID</MenuItem>
         <MenuItem onClick={handleSelect}>Contract Address</MenuItem>
       </Menu>
-    </div>
+    </>
   );
 }
