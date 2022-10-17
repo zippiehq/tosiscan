fb9ba8adf5d60f262fcd11b06738a617fe184f72
Olha Vadiasova <olha.vadiasova@gmail.com>
2022-10-17 15:38:09 +0300

    Fix styling, remove importing

--- src/components/header/Search.tsx
+++ src/components/header/Search.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { useNavigate, Navigate  } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 import { Menu, MenuItem, Button } from "@mui/material";
 import { KeyboardArrowDown, KeyboardArrowUp } from "@mui/icons-material";
