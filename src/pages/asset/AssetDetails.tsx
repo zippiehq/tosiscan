7f91a8f6588d39db49f5f98647a03f6e1d352d8d
Shola Ayeni <ayenisholah@yahoo.com>
2022-10-18 11:46:54 +0100

    fix timezone

--- src/pages/asset/AssetDetails.tsx
+++ src/pages/asset/AssetDetails.tsx
@@ -51,6 +51,7 @@ export default function AssetDetails() {
                     : moment(
                         moment
                           .unix(lastVerified)
+                          .utc()
                           .format("DD MMM YYYY HH:mm:ss [UTC]")
                       ).fromNow()}
                 </span>
