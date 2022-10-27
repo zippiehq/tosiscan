a8c179027628391e84503c13a19240b0934394e0
Maikoeliud <maikoeliud@gmail.com>
2022-10-27 13:21:08 +0300

    removed filters& changed placeholder

--- src/components/header/Search.tsx
+++ src/components/header/Search.tsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
-import { Menu, MenuItem, Button } from "@mui/material";
-import { KeyboardArrowDown, KeyboardArrowUp } from "@mui/icons-material";
+
 
 import { searchIcon } from "../../assets";
 
@@ -33,60 +32,13 @@ export default function SearchBar() {
         <img src={searchIcon} alt="Search Icon" />
         <input
           type="text"
-          placeholder="Asset URL / Asset ID / Contract / Keyword"
+          placeholder="Token URL"
           onChange={(ev) => {
             setSearchValue(ev.target.value)
           }}
         />
-        <BasicMenu />
+       
       </div>
     </form>
-  );
-}
-
-function BasicMenu() {
-  const [filter, setFilter] = useState("All filter");
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-
-  const open = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  const handleSelect = (event: any) => {
-    setFilter(event.target.outerText);
-    handleClose();
-  };
-
-  return (
-    <>
-      <Button
-        id="basic-button"
-        aria-controls={open ? "basic-menu" : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? "true" : undefined}
-        onClick={handleClick}
-        endIcon={open ? <KeyboardArrowUp /> : <KeyboardArrowDown />}
-      >
-        {filter}
-      </Button>
-      <Menu
-        id="basic-menu"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        MenuListProps={{
-          "aria-labelledby": "basic-button",
-        }}
-      >
-        <MenuItem onClick={handleSelect}>All Filter</MenuItem>
-        <MenuItem onClick={handleSelect}>Asset URL</MenuItem>
-        <MenuItem onClick={handleSelect}>Asset ID</MenuItem>
-        <MenuItem onClick={handleSelect}>Contract Address</MenuItem>
-      </Menu>
-    </>
   );
 }
