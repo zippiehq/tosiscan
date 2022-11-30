e1b137ecbe24a4481ac5d6292e93425be906b7c8
Maikoeliud <maikoeliud@gmail.com>
2022-11-30 09:29:03 +0300

    just a dash

--- src/components/DetailsTab.tsx
+++ src/components/DetailsTab.tsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom'
 
 import { Box, Typography, Table, TableBody, TableRow, TableContainer, TableCell } from '@mui/material'
 import { styled } from '@mui/system'
-import { formatDate } from '../utils/timestapFormater'
+import moment from 'moment'
 import { useTrustlessIndexingContext } from '../hooks/useTrustlessIndexing'
 
 const SectionWrapper = styled(Box)(({ theme }) => ({
@@ -47,9 +47,10 @@ const DetailsTab = () => {
 
   useEffect(() => setTLIQuery({ assetContract, assetTokenId }), [assetContract, assetTokenId, setTLIQuery])
   const token = TLIDataSet ? TLIDataSet.token : undefined
-  const dateValue = token?.mintTimestamp
-  const dateFormat = formatDate(dateValue / 1000)
-  const date = dateValue === null ? 'null' : dateFormat
+  const dateValue = token?.mintTimestamp as number
+  const dateTime = moment.unix(dateValue)
+  const dateFormat = dateTime.format('DD MMM YYYY HH:mm:ss [UTC]')
+  const date = dateValue === null ? '-' : dateFormat
 
   return (
     <>
