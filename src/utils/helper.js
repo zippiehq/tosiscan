20f1d32d26038678a314643bb755f56bca591935
Teddy <odenyothadeus@gmail.com>
2022-11-30 22:31:27 +0300

    Clickable link

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,6 +1,7 @@
 export function isValidUrl(str) {
   // eslint-disable-next-line no-useless-escape
   const res = str.match(
+    // eslint-disable-next-line no-useless-escape
     /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g,
   )
   return res !== null
