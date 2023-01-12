82bbd9f3cb3fa1ddd5e44971d5a7015fb18f151f
Maikoeliud <maikoeliud@gmail.com>
2023-01-12 12:14:16 +0300

    good fix for not found issue

--- src/views/AssetSearchResult.tsx
+++ src/views/AssetSearchResult.tsx
@@ -126,7 +126,7 @@ const AssetSearchResult = () => {
     )
   })
 
-  if ((!assetContract || !assetTokenId) && !datasetId && !TLIDataSet && !datasets.length) {
+  if ((!assetContract || !assetTokenId || !datasetId) && !TLIDataSet && !datasets.length) {
     return (
       <ContainerWithoutData>
         <Typography variant="body2" sx={{ fontSize: '20px' }}>
